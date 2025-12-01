053834f86594a9606260ae911aa0fa6d9d54eeb1
OmarMarg <omarmtzarg@gmail.com>
2025-12-01 16:19:42 -0600

    Se detallan más los estilos y se agrega información a Tech Stack

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const btnCopy = document.getElementById("btnCopyEmail");
 const email = btnCopy.textContent;
 
+
 // Función para copiar texto al portapales
 btnCopy.addEventListener("click", () => {
     navigator.clipboard.writeText(email)
@@ -15,3 +16,15 @@ btnCopy.addEventListener("click", () => {
         })
         .catch(err => console.error("Error al copiar: ", err));
 });
+
+// Función para activar el fade y blur cuando se haga scroll a la página 
+// const navbar = document.querySelector(".fade-navbar");
+/*
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 10) {
+        navbar.classList.add("show");
+    } else {
+        navbar.classList.remove("show");
+    }
+});
+*/
